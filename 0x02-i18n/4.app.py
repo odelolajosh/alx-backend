da3c1267a46a6de96f21a22c709ce5b535db9f1d
odelolajosh <oladapoodelola@gmail.com>
2023-02-28 15:56:49 +0100

    task: force local with url parameter

---
+++ 0x02-i18n/4.app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" 2. get local from request """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Config class for app """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Get locale from request """
+    query_strings = request.query_string.decode('utf-8').split('&')
+    query = {k: v for k, v in [i.split('=') for i in query_strings]}
+    if 'locale' in query and query['locale'] in app.config["LANGUAGES"]:
+        return query['locale']
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def welcome() -> str:
+    """ / page """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
