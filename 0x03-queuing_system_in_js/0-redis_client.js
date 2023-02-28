eb7f43cbbdfb5d7ef740e14927c079aeb3db23a8
odelolajosh <oladapoodelola@gmail.com>
2023-02-28 14:56:53 +0100

    task: redis node and basic operations

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,18 @@
+import { createClient } from 'redis';
+
+/**
+ * Connect to the Redis server
+*/
+
+const client = createClient();
+
+client.on('error', (err) => {
+  const msg = err.message || String(err);
+  console.log(`Redis client not connected to the server: ${msg}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.connect();
